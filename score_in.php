4220e25263cb0180d4039389ffe0628c96fb44fc
00LT00 <lightning@hdu.edu.cn>
2019-05-05 14:39:15 +0800

    对开关进行判断

--- score_in.php
+++ score_in.php
@@ -3,8 +3,24 @@ header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Request-Methods:GET, POST, PUT, DELETE, OPTIONS");
 header('Access-Control-Allow-Headers:content-type');
+ini_set("display_errors", "on");
 if($_SERVER['REQUEST_METHOD']=='OPTIONS'){
     die();
+}
+function enable($stage){
+    include "conn.php";
+    date_default_timezone_set('PRC');  //设置默认时区 PRC(中华人民共和国)
+    $time=strtotime(date("Y-m-d H:i:s"));
+    $sql = "select enable_time from team where stage = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($stage));
+    $sql_arr = $stmt->fetch(PDO::FETCH_ASSOC);
+    $opentime = strtotime($sql_arr['enable_time']);
+    if ($time - $opentime < 120) {
+        return true;
+    }else{
+        return false;
+    }
 }
 include "conn.php";
 ini_set("display_errors", "off");
@@ -16,6 +32,13 @@ if($json_string !== ' '){
     if (is_null($stage)){
         http_response_code(403);
         $str = array('error' => '40302', 'msg' =>'insert is faile');
+        echo json_encode($str);
+        exit;
+    }
+    /**判断投票是否开启 */
+    if (!enable($stage)) {
+        http_response_code(403);
+        $str = array('error' => '40303', 'msg' =>'time limit');
         echo json_encode($str);
         exit;
     }
